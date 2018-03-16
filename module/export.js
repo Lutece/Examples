7616e6133ab45922aec06be3e665ae88fc26b8f5
lutece <lallaworld1st@gmail.com>
2018-03-17 05:00:50 +0900

    module-export

---
+++ module/export.js
@@ -0,0 +1,12 @@
+const  myFavoriteFruitList  = ['apple', 'banana'];
+const  myFavoriteVagetableList  = ['tomato', 'eggplant'];
+
+// export { myFavoriteFruitList, myFavoriteVagetableList };
+// export { myFavoriteFruitList as myFruits, myFavoriteVagetableList as myVagetables };
+
+// export function myFruitsToString() {
+// 	return myFavoriteFruitList.toString();
+// };
+
+// export { myFavoriteFruitList as default };
+export default myFavoriteFruitList; //위와 같습니다.
